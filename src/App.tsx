93092dedc2ec174321ad15617c310930b0e71521
Royal Finch <Royal2791@gmail.com>
2024-05-14 20:33:17 -0400

    feat: :art: add initial html structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,21 @@
 export default function App() {
-  return <div className="">Hello World!</div>;
+  return (
+    <main>
+      <img src="/images/image-equilibrium.jpg" alt="" />
+      <h2>Equilibrium #3429</h2>
+      <p>Our Equilibrium collection promotes balance and calm.</p>
+      <div>
+        <img src="/images/icon-ethereum.svg" alt="" />
+        <span>0.041 ETH</span>
+        <img src="/images/icon-clock.svg" alt="" />
+        <span>3 days left</span>
+      </div>
+      <div>
+        <img src="/images/image-avatar.png" alt="Jules Wyvern avatar" />
+        <span>
+          Creation of <span>Jules Wyvern</span>
+        </span>
+      </div>
+    </main>
+  );
 }
