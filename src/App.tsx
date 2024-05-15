92e9f1c2f37a081a241e944b3ba1889307907f38
Royal Finch <Royal2791@gmail.com>
2024-05-14 21:18:18 -0400

    feat: :lipstick: add initial styling to card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
 export default function App() {
   return (
-    <main>
-      <img src="/images/image-equilibrium.jpg" alt="" />
-      <h2>Equilibrium #3429</h2>
-      <p>Our Equilibrium collection promotes balance and calm.</p>
-      <div>
-        <img src="/images/icon-ethereum.svg" alt="" />
-        <span>0.041 ETH</span>
-        <img src="/images/icon-clock.svg" alt="" />
-        <span>3 days left</span>
-      </div>
-      <div>
-        <img src="/images/image-avatar.png" alt="Jules Wyvern avatar" />
-        <span>
-          Creation of <span>Jules Wyvern</span>
-        </span>
+    <main className="bg-main-bg  min-h-screen px-6 pt-16 text-white">
+      <h1 className="sr-only">NFT Preview Card</h1>
+      <div className="bg-card-bg mx-auto max-w-96 rounded-2xl p-6">
+        <img
+          className=" mb-8 rounded-xl"
+          src="/images/image-equilibrium.jpg"
+          alt=""
+        />
+
+        <h2 className="font-outfit-600 pb-4 text-2xl">Equilibrium #3429</h2>
+        <p className="text-soft-blue text-m pb-6">
+          Our Equilibrium collection promotes balance and calm.
+        </p>
+        <div className="flex items-center">
+          <img className="h-5 pr-2" src="/images/icon-ethereum.svg" alt="" />
+          <span className="text-cyan font-outfit-600 grow">0.041 ETH</span>
+          <img className="h-5 pr-2" src="/images/icon-clock.svg" alt="" />
+          <span className="text-soft-blue">3 days left</span>
+        </div>
+        <div className="border-soft-blue my-4 border-b border-opacity-30"></div>
+        <div className=" flex items-center">
+          <img
+            className="mr-4 h-10 rounded-full border"
+            src="/images/image-avatar.png"
+            alt="Jules Wyvern avatar"
+          />
+          <span className="text-soft-blue">
+            Creation of <span className="text-white">Jules Wyvern</span>
+          </span>
+        </div>
       </div>
     </main>
   );
